da9012327aba7b8447f837c9b5280bd792ce5599
Vipul <81670997+Vipul1432@users.noreply.github.com>
2022-05-07 23:08:22 +0530

    Conditional Statement

---
+++ 9_passfail.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int m;
+    printf("Enter the marks : ");
+    scanf("%d", &m);
+    if (m > 33)
+    {
+        printf("pass\n");
+    }
+    else
+    {
+        printf("fail\n");
+    }
+}
