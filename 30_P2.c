655bbaede96e75682dee90edfebab02f4724e73b
Vipul <81670997+Vipul1432@users.noreply.github.com>
2022-06-03 14:28:47 +0530

    Number Pattern 2

---
+++ 30_P2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+    int i, j, rows;
+    printf("Enter number of rows : ");
+    scanf("%d", &rows);
+    for (i = rows; i >= 1; i--)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+}
