a7a201d11f29bb4212e32d9f05e6315e8c912ddc
Vipul <81670997+Vipul1432@users.noreply.github.com>
2022-06-03 14:30:02 +0530

    Number Pattern 4

---
+++ 30_P4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+    int i, j, rows;
+
+    printf("Enter number of rows : ");
+    scanf("%d", &rows);
+
+    for (i = 1; i <= rows; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d ", i);
+        }
+        printf("\n");
+    }
+}
