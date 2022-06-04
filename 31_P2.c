390e602179fe6354f6ee700167d6cd5feff2b95a
Vipul <81670997+Vipul1432@users.noreply.github.com>
2022-06-04 10:41:41 +0530

    Pattern Alphabet 2

---
+++ 31_P2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+    int i, j, rows;
+    printf("Enter number of rows  ");
+    scanf("%d", &rows);
+    for (i = rows; i >= 1; i--)
+    {
+        char c = 'A';
+        for (j = 1; j <= i; j++)
+        {
+            printf("%c", c);
+            c++;
+        }
+        printf("\n");
+    }
+}
